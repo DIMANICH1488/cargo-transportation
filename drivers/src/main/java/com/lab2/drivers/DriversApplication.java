d79c03e837fe81e42908aee3802ce682b0a1b5b5
Dima <dima2001best@i.ua>
2021-12-19 03:37:59 +0200

    driver microservice init

---
+++ drivers/src/main/java/com/lab2/drivers/DriversApplication.java
@@ -0,0 +1,13 @@
+package com.lab2.drivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriversApplication.class, args);
+	}
+
+}

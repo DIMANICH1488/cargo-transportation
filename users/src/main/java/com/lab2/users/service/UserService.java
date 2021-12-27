5d295c92464064922ff676af4de681cdeb9d312b
Dima <dima2001best@i.ua>
2021-12-27 02:53:23 +0200

    experiments

--- users/src/main/java/com/lab2/users/service/UserService.java
+++ users/src/main/java/com/lab2/users/service/UserService.java
@@ -3,9 +3,13 @@ package com.lab2.users.service;
 import com.lab2.users.repo.UserRepo;
 import com.lab2.users.repo.model.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RequiredArgsConstructor
